0ec001a834e1b93721272c8d889cb191289e55f2
mohammadshahidbeigh <mohammadshahidbeigh@gmail.com>
2025-03-17 23:00:59 +0530

    Refactor middleware for authentication handling: Replaced withAuth from next-auth with a custom auth function to manage login redirects and session validation. Updated request path matching to exclude specific API and static routes.

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,15 +1,34 @@
-import { withAuth } from "next-auth/middleware"
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { auth } from '@/app/auth'
+import type { Session } from 'next-auth'
 
-export default withAuth({
-  callbacks: {
-    authorized: ({ token }) => !!token
-  },
+export default auth((req: NextRequest & { auth: Session | null }) => {
+  const isLoggedIn = !!req.auth
+  const isOnLoginPage = req.nextUrl.pathname.startsWith('/login')
+  
+  if (isOnLoginPage) {
+    if (isLoggedIn) {
+      return NextResponse.redirect(new URL('/', req.url))
+    }
+    return NextResponse.next()
+  }
+
+  if (!isLoggedIn) {
+    return NextResponse.redirect(new URL('/login', req.url))
+  }
+  return NextResponse.next()
 })
 
 export const config = {
   matcher: [
-    '/',
-    '/api/instant-meeting',
-    '/api/schedule-meeting'
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api/auth (auth API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api/auth|_next/static|_next/image|favicon.ico).*)'
   ]
 } 
